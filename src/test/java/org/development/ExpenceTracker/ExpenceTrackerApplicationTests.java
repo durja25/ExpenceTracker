55afab1682fe5bcf497ce1a010e067cde60a2541
SAM Manager <kganesh00708@gmail.com>
2026-01-27 11:49:12 +0530

    Initial commit

---
+++ src/test/java/org/development/ExpenceTracker/ExpenceTrackerApplicationTests.java
@@ -0,0 +1,13 @@
+package org.development.ExpenceTracker;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ExpenceTrackerApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
